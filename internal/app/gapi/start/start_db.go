027d41d3c21eb30c80bed61e299f36c40132c51b
dicoder-home-mac <dicoder@126.com>
2025-08-23 01:20:01 +0800

    优化 db

--- internal/app/gapi/start/start_db.go
+++ internal/app/gapi/start/start_db.go
@@ -2,23 +2,17 @@ package start
 
 import (
 	"fmt"
-	"ginp-api/configs"
-	"ginp-api/internal/db/mysql"
+	"ginp-api/internal/db/dbs"
 )
 
 func startDB() {
-	mysql.InitDb(
-		configs.MysqlIp(),
-		configs.MysqlPort(),
-		configs.MysqlUser(),
-		configs.MysqlDb(),
-		configs.MysqlPwd(),
-	)
+	// 初始化PostgreSQL数据库
+	dbs.InitDb(dbs.DbTypePgsql)
 
 	//迁移表
-	if mysql.GetWriteDb() != nil {
+	if dbs.GetWriteDb() != nil {
 		//自动迁移表结构
-		err := mysql.GetWriteDb().AutoMigrate(EntityAutoMigrateList...)
+		err := dbs.GetWriteDb().AutoMigrate(EntityAutoMigrateList...)
 		if err != nil {
 			fmt.Println("迁移表结构失败" + err.Error())
 			panic(err)
---
+++ internal/db/dbs/get_db.go
@@ -0,0 +1,56 @@
+package dbs
+
+import (
+	"ginp-api/internal/db/mysql"
+	"ginp-api/internal/db/pgsql"
+	"ginp-api/internal/db/sqlite"
+
+	"gorm.io/gorm"
+)
+
+const (
+	DbTypeMysql  = "mysql"
+	DbTypePgsql  = "pgsql"
+	DbTypeSqlite = "sqlite"
+)
+
+const useDbType = DbTypePgsql
+
+var (
+	DbRead  *gorm.DB
+	DbWrite *gorm.DB
+)
+
+func GetReadDb() *gorm.DB {
+	switch useDbType {
+	case DbTypeMysql:
+		return mysql.GetReadDb()
+	case DbTypePgsql:
+		return pgsql.GetReadDb()
+	case DbTypeSqlite:
+		db, err := sqlite.GetReadDb()
+		if err != nil {
+			panic(err)
+		}
+		return db
+	default:
+		return mysql.GetReadDb()
+	}
+}
+
+func GetWriteDb() *gorm.DB {
+	switch useDbType {
+	case DbTypeMysql:
+		return mysql.GetWriteDb()
+	case DbTypePgsql:
+		return pgsql.GetWriteDb()
+	case DbTypeSqlite:
+		db, err := sqlite.GetWriteDb()
+		if err != nil {
+			panic(err)
+		}
+		return db
+	default:
+		return mysql.GetWriteDb()
+	}
+}
